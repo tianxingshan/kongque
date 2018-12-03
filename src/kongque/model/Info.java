8f3f3e58f71e2dbdae142a964aa068dc3261d606
tianxingshan <602926338@qq.com>
2018-12-03 22:24:02 +0800

    最后一次修改

---
+++ src/kongque/model/Info.java
@@ -0,0 +1,27 @@
+package kongque.model;
+
+public class Info {
+	
+	private String price;
+	
+	private String time;
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+
+}
