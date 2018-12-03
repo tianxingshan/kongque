86f4c6a7a684e6877bebea598dc237ced397ba9a
tianxingshan <602926338@qq.com>
2018-12-03 22:29:28 +0800

    修改信息实体类

--- src/kongque/model/Info.java
+++ src/kongque/model/Info.java
@@ -1,10 +1,22 @@
 package kongque.model;
 
+import java.util.Date;
+
 public class Info {
 	
 	private String price;
 	
 	private String time;
+	
+	private Date date;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	public String getPrice() {
 		return price;
